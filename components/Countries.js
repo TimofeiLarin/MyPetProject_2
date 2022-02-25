7f73d1e30a1525f975b4295979f1469f2ab82f57
Timofei Larin <timofeilarine31@gmail.ru>
2022-02-25 19:25:27 +0300

    added a function to display countries and select it, added stiles for component Container

--- components/Countries.js
+++ components/Countries.js
@@ -4,21 +4,44 @@ import { useGetCountries } from '../lib/hooks';
 
 const Countries = () => {
   const [country, setCountry] = useState('');
-  const data = useGetCountries();
+  const [countrySelected, setCountrySelected] = useState('');
+  const [countryOptions, setCountryOptions] = useState([]);
+  const { data: countries } = useGetCountries();
+  const handleClick = (name) => {
+    setCountry(name);
+    setCountryOptions([]);
+    setCountrySelected(name);
+  };
   const handleInput = (e) => {
-    if (e.target.value){
+    if (e.target.value) {
       setCountry(e.target.value);
+      const options = countries.filter((res) => {
+        const regex = new RegExp(e.target.value, 'gi');
+        return res.name.match(regex);
+      });
+      setCountryOptions(options);
     } else {
       setCountry('');
+      setCountryOptions([]);
     }
-  }
-  console.log(country);
+  };
+  const renderCountry = (flagUrl, name, id) => {
+    return flagUrl ? (
+      <div className='option' key={id} onClick={() => handleClick(name)}>
+        <img src={flagUrl} alt={name} />
+        <span>{name}</span>
+      </div>
+    ) : null;
+  };
 
   return (
     <Container>
       <div className='form'>
         <h2>Countries</h2>
-        <input type='text' value={country} onChange={handleInput}/>
+        <input type='text' value={country} onChange={handleInput} />
+        {!!countryOptions.length && (
+          <div>{countryOptions.map((res) => renderCountry(res.media.flag, res.name, res.id))}</div>
+        )}
       </div>
     </Container>
   );
@@ -27,20 +50,31 @@ const Countries = () => {
 export default Countries;
 
 const Container = styled.div`
-  display:flex;
-  justify-content: center;
+  display: flex;
+  justify-content: space-around;
   max-width: 610px;
   margin: 50px auto 0;
-  background-color: #5DFC68;
-  border: 5px solid #A60003;
+  background-color: #dcf2f0;
+  border: 5px solid #a60003;
   border-radius: 5px;
   .form {
     width: 210px;
-    min-height:26px;
-    border: 3px solid #A60003;
+    min-height: 26px;
+    border: 3px solid #a60003;
     border-radius: 5px;
     margin: 10px 0;
     padding: 5px;
     font-size: 16px;
+    max-height: 300px;
+    overflow: auto;
+  }
+  .option {
+    border-bottom: 1px solid #a60003;
+    padding: 5px;
+    cursor: pointer;
+    img {
+      max-width: 30px;
+      margin-right: 10px;
+    }
   }
 `;
